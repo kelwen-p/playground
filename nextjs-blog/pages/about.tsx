46e884c47de448bfdcd223478b82d26d3eb637cb
Kelwen Peng <kelwen.p@gmail.com>
2021-11-03 20:45:37 -0400

    App and nextjs-blog apps created

---
+++ nextjs-blog/pages/about.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+const About = () => {
+    return (
+    <>
+        <div>About</div>
+        <Button>Button</Button>
+    </>  
+    );
+}
+  
+export default About
